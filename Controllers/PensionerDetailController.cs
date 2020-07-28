bf06f8c29709c340e05e57595a4918eb680f4509
sahil-nayak <65059575+sahil-nayak@users.noreply.github.com>
2020-07-28 10:52:15 +0530

    code added

---
+++ Controllers/PensionerDetailController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MFRP_Pension_Detail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PensionerDetailController : ControllerBase
+    {
+        private List<PensionerDetail> pensiondetail = new List<PensionerDetail>();
+
+        public PensionerDetailController(List<PensionerDetail> pensiondetails)
+        {
+            pensiondetail = pensiondetails;
+        }
+        // GET: api/PensionerDetail
+        [HttpGet]
+        public IEnumerable<PensionerDetail> Get()
+        {
+            return pensiondetail.ToList();
+        }
+
+        // GET: api/PensionerDetail/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        
+    }
+}
---
+++ PensionerDetail.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MFRP_Pension_Detail
+{
+    public class PensionerDetail
+    {
+        public string name { get; set; }
+        public DateTime date_of_birth{get; set; }
+        public string pan { get; set; }
+        public int salaryEarned { get; set; }
+        public int allowances { get; set; }
+        public string aadharNumber { get; set; }
+        public PensionType pensionType { get; set; }
+        public string bankName { get; set; }
+        public string accountNumber { get; set; }
+        public BankType bankType { get; set; }
+
+
+    }
+
+    public enum PensionType
+    {
+        Self=1,
+        Family=2
+    }
+    public enum BankType
+    {
+        Public=1,
+        Private=2
+    }
+
+    
+}
